fe1e8253d6e35945b23ee5331070dbdb9d694cc3
Eric Stasney <ericstasney@gmail.com>
2018-01-20 15:03:50 -0500

    Update row2csv

--- mysite/csv_parser.py
+++ mysite/csv_parser.py
@@ -1,6 +1,57 @@
 import pandas as pd
+import base64
+import pickle
 
 def db_to_csv(data):
-    # TODO Fullname
-    # TODO Website
 
+
+    with open(r"/home/eric/PycharmProjects/FlaskAPI/mysite/country_codes.pkl", "rb") as cc:
+        country_dict = pickle.load(cc)
+    df = pd.DataFrame(data)
+    df2 = pd.DataFrame(columns=['Full Name', 'First Name', 'Last Name', 'Metropolitan Area',
+                                'Home State', 'Home Postal Code', 'Home Country', 'Theater', 'Skills and Technologies',
+                                'Company', 'Position Title', 'Prior Employer', 'Prior Position Title',
+                                'Work History - Company', 'Work History - Position title', 'Home Email', 'Work Email',
+                                'Mobile Phone', 'Home Phone', 'Work Phone', 'Summary', 'Website', 'Source',
+                                'Base64-encoded attachment Name', 'Base64-encoded attachment content'])
+    df2['Full Name'] = df['first_name'] + " " + df['last_name']
+    df2['First Name'] = df['first_name']
+    df2['Last Name'] = df['last_name']
+    df2['Metropolitan Area'] = df['metro']
+    df2['Home Postal Code'] = df['postal_code'].astype(str)
+    df2['Home Country'] = df['country_code'].apply(lambda x: country_dict[x]) # Use country code dict
+    df2['Skills and Technologies'] = df['skills']
+    df2['Company'] = df['companyName_0']
+    df2['Position Title'] = df['title_0']
+    df2['Prior Employer'] = df['companyName_1']
+    df2['Prior Position Title'] = df['title_1']
+    df2['Work History - Company'] = df['companyName_2']
+    df2['Work History - Position title'] = df['title_2']
+    df2['Summary'] = df['summary']
+    df2['Base64-encoded attachment Name'] = df['member_id'] + ".rtf"
+    df2['Base64-encoded attachment content'] = df.apply(make_resume, axis=1)
+
+    return df2.to_csv(index=False)
+
+# Content
+
+def make_resume(row):
+    break_line = "-----------"
+    resume = "{first} {last}\n{location}\nSummary{summary}\n{break_1}\n" \
+             "Experience\n\n" \
+             "{title_0} at {companyName_0}\n" \
+             "{start_date_0} - Present\n" \
+             "{summary_0}\n\n" \
+             "{title_1} at {companyName_1}\n" \
+             "{start_date_1} - {end_date_1}\n" \
+             "{summary_1}\n\n" \
+             "{title_2} at {companyName_2}\n" \
+             "{start_date_2} - {end_date_2}" \
+             "{summary_2}".format(first=row.first_name,location=row.metro, last=row.last_name, summary=row.summary,
+                                  break_1=break_line, title_0=row.title_0, companyName_0=row.companyName_0,
+                                  start_date_0=row.start_date_0, summary_0=row.summary_0, title_1=row.title_1,
+                                  companyName_1=row.companyName_1, start_date_1=row.start_date_1,
+                                  end_date_1=row.end_date_1, summary_1=row.summary_1, title_2=row.title_2,
+                                  companyName_2=row.companyName_2, start_date_2=row.start_date_2,
+                                  end_date_2=row.end_date_2, summary_2=row.summary_2)
+    return base64.b64encode(resume.encode())
