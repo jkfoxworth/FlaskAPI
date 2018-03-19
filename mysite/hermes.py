a32ce0768bbf131617bf7a10cf09ddb9f0d78433
estasney <estasney@users.noreply.github.com>
2018-03-19 14:04:01 -0400

    expand shell context

--- mysite/hermes.py
+++ mysite/hermes.py
@@ -1,8 +1,9 @@
 from app_folder import app_run, db
-from app_folder.models import User
+from app_folder.models import User, LinkedInRecord, UserCache, Contact
 from werkzeug.security import generate_password_hash
 
 
 @app_run.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User, 'generate_password_hash': generate_password_hash}
+    return {'db': db, 'User': User, 'generate_password_hash': generate_password_hash, 'LinkedInRecord': LinkedInRecord,
+            'UserCache': UserCache, 'Contact': Contact}
