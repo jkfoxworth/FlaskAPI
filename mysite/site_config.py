f8ee0953a3b5d647de7751d50d3786d94c73c736
estasney <estasney@users.noreply.github.com>
2018-03-19 13:16:20 -0400

    remove duplicate options from rebase

--- mysite/site_config.py
+++ mysite/site_config.py
@@ -16,11 +16,8 @@ class Config(object):
     TRAP_BAD_REQUEST_ERRORS = True
     TRAP_HTTP_EXCEPTIONS = True
     USE_DEBUGGER = False
-    DEBUG_TB_INTERCEPT_REDIRECTS = False
     USE_RELOADER = False
     UPLOAD_FOLDER = os.path.join(basedir, 'app_folder/uploads')
-    USE_DEBUGGER = False
-    USE_RELOADER = False
 
 
 class FConfig(object):
